43b0b2de236dbab16f01e8a08335b1a09531cd43
Edu <edwinsp123@gmail.com>
2019-03-28 22:48:45 -0500

    Update config.inc.php

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -15,7 +15,7 @@
 
 	// Hostname or IP address for server.  Use '' for UNIX domain socket.
 	// use 'localhost' for TCP/IP connection on this computer
-	$conf['servers'][0]['host'] = 'https://postgresql-prypostgres.7e14.starter-us-west-2.openshiftapps.com/';
+	$conf['servers'][0]['host'] = 'https://10.130.22.154';
 
 	// Database port on server (5432 is the PostgreSQL default)
 	$conf['servers'][0]['port'] = 5432;
@@ -28,7 +28,7 @@
 
 	// Change the default database only if you cannot connect to template1.
 	// For a PostgreSQL 8.1+ server, you can set this to 'postgres'.
-	$conf['servers'][0]['defaultdb'] = 'sampledb';
+	$conf['servers'][0]['defaultdb'] = 'tropicodb';
 
 	// Specify the path to the database dump utilities for this server.
 	// You can set these to '' if no dumper is available.
