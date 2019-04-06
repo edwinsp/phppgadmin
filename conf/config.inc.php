782de0867e422ad0e282223046900270940b9281
Edu <edwinsp123@gmail.com>
2019-04-06 11:59:05 -0500

    Update config.inc.php

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -15,7 +15,7 @@
 
 	// Hostname or IP address for server.  Use '' for UNIX domain socket.
 	// use 'localhost' for TCP/IP connection on this computer
-	$conf['servers'][0]['host'] = 'postgresql';									
+	$conf['servers'][0]['host'] = 'jws-app-postgresql';									
 
 	// Database port on server (5432 is the PostgreSQL default)
 	$conf['servers'][0]['port'] = 5432;
